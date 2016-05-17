2e1197ed7a28c97b80717c47400678f156162eec
Nicholas Thomson <neekthomson@gmail.com>
2016-05-16 21:21:46 -0400

    Fix bug by reseting page fault count after each algo

--- classes/VirtualMemSim.py
+++ classes/VirtualMemSim.py
@@ -14,11 +14,14 @@ class VirtualMemSim:
             self.reference_page(i, "OPT")
         print "End of OPT simulation (%d page faults)" % self.page_fault_count
         self.frame_list = self.empty_frame_list()
+        self.page_fault_count = 0
 
         print "\nSimulating LRU with fixed frame size of %d" % self.frame_size
         for i in range(0, len(self.page_ref_list)):
             self.reference_page(i, "LRU")
         print "End of LRU simulation (%d page faults)" % self.page_fault_count
+        self.frame_list = self.empty_frame_list()
+        self.page_fault_count = 0
 
         print "\nSimulating LFU with fixed frame size of %d" % self.frame_size
         for i in range(0, len(self.page_ref_list)):
